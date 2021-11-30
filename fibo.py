35473c98ac7f0315908757f8f2fd90dbe476464e
Lucas Vinicius <88403508+Lucasmcz@users.noreply.github.com>
2021-11-30 08:28:08 -0300

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -1,18 +1,54 @@
-from matplotlib import pyplot as plt
-n = int(input("Que n-enesimo termo deseja encontrar: "))
-ultimo=1
-penultimo=1
+import time
+import timeit
+import matplotlib.pyplot as plt
+import numpy as np
 
-if (n==1) or (n==2):
-    print("1")
-else:
-    for count in range(2,n):
-        termo = ultimo + penultimo
-        penultimo = ultimo
-        ultimo = termo
-        count += 1
-    print(termo)
+#Algoritmo Fibonnaci Recursivo
+def alguma_funcao(n):
+    ultimo=1
+    penultimo=1
 
-    
+    if (n==1) or (n==2):
+        return 
+    else:
+       for count in range(2,n):
+            termo = ultimo + penultimo
+            penultimo = ultimo
+            ultimo = termo
+            count += 1
+
+
+
+inicio = timeit.default_timer()
+alguma_funcao(1)
+fim = timeit.default_timer()
+valor = (fim - inicio)
+
+inicio1 = timeit.default_timer()
+alguma_funcao(1000)
+fim1 = timeit.default_timer()
+valor1 = (fim1 - inicio1)
+
+
+
+inicio2 = timeit.default_timer()
+alguma_funcao(10000)
+fim2 = timeit.default_timer()
+valor2 = (fim2 - inicio2)
+
+
+
+inicio3 = timeit.default_timer()
+alguma_funcao(200000)
+fim3 = timeit.default_timer()
+valor3 = (fim3 - inicio3)
+
+arr_n = [valor,valor1,valor2,valor3]
+
+print(arr_n)
+plt.plot(arr_n,[1,1000,10000,20000])
+plt.title("Grafico do Algoritmo Iterativo")
+plt.show()
+#print('duracao do iterativo: %f' % (fim - inicio))
 
 
