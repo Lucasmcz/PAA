685a3fc6a9bdf3282731dc93720417586aefc9a7
Lucas Vinicius <88403508+Lucasmcz@users.noreply.github.com>
2021-11-30 08:29:08 -0300

    Update fibo_rec.py

--- fibo_rec.py
+++ fibo_rec.py
@@ -1,14 +1,43 @@
-from matplotlib import pyplot as plt
-n = int(input("Que n-enesimo termo deseja encontrar: "))
-def fibonnaci(n):
-    if(n == 1):
-        return 1
-    elif( n== 2):
-        return 1
-    
-    else:
-        return fibonnaci(n-1) + fibonnaci(n-2)
-    
-    
-    
-print(fibonnaci(n))
+import time
+import timeit
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Algoritmo Fibonnaci Recursivo
+def fibonnaci(n):                            
+    if n<=1:                                 
+        return n                             
+    else:                                    
+        return (fibonnaci(n-1)+fibonnaci(n-2))
+
+
+
+inicio = timeit.default_timer()
+fibonnaci(1)
+fim = timeit.default_timer()
+valor = (fim - inicio)
+
+
+inicio1 = timeit.default_timer()
+fibonnaci(10)
+fim1 = timeit.default_timer()
+valor1 = (fim1 - inicio1)
+
+inicio2 = timeit.default_timer()
+fibonnaci(20)
+fim2 = timeit.default_timer()
+valor2 = (fim2 - inicio2)
+
+inicio3 = timeit.default_timer()
+fibonnaci(30)
+fim3 = timeit.default_timer()
+valor3 = (fim3 - inicio3)
+
+arr_n = [valor,valor1,valor2,valor3]
+
+print(arr_n)
+plt.plot(arr_n,[1,10,20,30])
+plt.title("Grafico do Algoritmo Recursivo")
+plt.show()
+
+
